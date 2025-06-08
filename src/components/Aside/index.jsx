dfef81ffeaab072f3af38ed1e8826c71cd5b2bba
GabrielAraujoNeves <eg69228@gmail.com>
2025-06-07 23:18:35 -0400

    foi ajustado as imagens no código e ajustado o Next

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -6,6 +6,6 @@ import logo from './logo.png'
 export const Aside = () => {
     return (<aside className={styles.aside}>
         {/* <img src="/logo.png" alt="Logo da Code Connect" /> */}
-        <Image src={logo} alt="Logo da Code Connect"/>
+        <Image src={logo} alt="Logo da Code Connect" />
     </aside>)
 }
--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -1,10 +1,14 @@
 import Image from "next/image"
 
-export const Avatar = ({name, imageSrc}) => {
+export const Avatar = ({ name, imageSrc }) => {
     return (
         <ul>
             <li>
-              <Image src={imageSrc}/>
+                <Image
+                    src={imageSrc}
+                    width={32}
+                    height={32}
+                    alt={`Avatar do(a) ${name}`} />
             </li>
             <li>
                 @{name}
